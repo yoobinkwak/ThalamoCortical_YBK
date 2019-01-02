e9ca368a7c2b13b07e1bdaf672c4fa70cb6643e6
yoobinkwak <yoobinkwak@gmail.com>
2019-01-02 16:56:17 +0900

    Add files via upload

---
+++ 40FEP_40NOR/5_rsfMRI_tractography.py
@@ -0,0 +1,178 @@
+import os
+from os.path import join, basename, dirname, isfile, isdir
+import re
+from multiprocessing import Pool
+import sys
+
+
+###### same script as rsFC_seed_tractography.py (i.e., just different script name)
+
+dataLoc = '/Volumes/CCNC_3T_1/rsFC_to_wmSC'
+sub_dir = [ 'ds3_bi_fwhm6_10ICs', 'ds3_bi_fwhm6preproc_10ICs', 'ds3_bi_nosmooth_10ICs', 'ds3_bi_fwhm6_20ICs',  'ds3_bi_fwhm6preproc_20ICs', 'ds3_bi_nosmooth_20ICs']
+
+
+
+class subject:
+    def __init__(self, subject):
+        self.gpu_num = 0
+        self.subject = subject
+        self.subject_dir = join(dataLoc, subject)
+        ##self.segmentation_dir = join(self.subject_dir, 'segmentation')
+        self.out_dir = join(self.subject_dir, 'rsFC_seed_tractography')
+        self.bedpost_dir = join(self.subject_dir, 'DTI.bedpostX')
+        self.reg_dir = join(self.subject_dir, 'DTI/Registration')
+        ##self.roi_dir = join(self.subject_dir, 'ROI')
+        ##self.left_seg_dir = join(self.segmentation_dir, 'left')
+        ##self.right_seg_dir = join(self.segmentation_dir, 'right')
+#        for subdir in sub_dir:
+#            self.output_dir = join(self.out_dir, '{}'.format(subdir))
+
+
+def run_commands(subject_class):
+
+    commands = []
+
+    for subdir in sub_dir:
+        for outdir in [subject_class.out_dir]:
+            if not os.path.exists(outdir):
+                os.mkdir(outdir)
+            directory = '{}/{}'.format(outdir, subdir)
+            if not os.path.exists(directory):
+                os.mkdir(directory)
+
+        nocov_input_dir = 'rsFC_Randomise_n80/{}/two_step_randomise/nocov_1_sample_tfce/sig_clusters'.format(subdir)
+        input_dir = 'rsFC_Randomise_n80/{}/two_step_randomise/1_sample_tfce/sig_clusters'.format(subdir)
+        
+        nocov_imgs = [x for x in os.listdir(nocov_input_dir) if isfile(join(nocov_input_dir, x))]
+        imgs = [y for y in os.listdir(input_dir) if isfile(join(input_dir, y))]
+
+        for nocov_img in nocov_imgs:
+            nocov_filename = ''.join(nocov_img.replace('t0.95_concatenated_ds3_bi_',' ').replace('.nii.gz',' ').split())
+            nocov_tract_dir = '{}/{}'.format(directory, nocov_filename)
+            #nocov_thal_roi = join(nocov_input_dir, nocov_img)
+            
+            command = 'CUDA_VISIBLE_DEVICES={gpu_num} /usr/local/fsl/bin/probtrackx2_gpu \
+                -x {nocov_thal_roi} \
+                -l \
+                --onewaycondition \
+                --omatrix2 \
+                -c 0.2 \
+                -S 2000 \
+                --steplength=0.5 \
+                -P 5000 \
+                --fibthresh=0.01 \
+                --distthresh=0.0 \
+                --sampvox=0.0 \
+                --forcedir \
+                --opd \
+                -s {bedpostDir}/merged \
+                -m {bedpostDir}/nodif_brain_mask \
+                --xfm={reorient_mni2t1w2nodif} \
+                --invxfm={reorient_nodif2t1w2mni} \
+                --dir={tract_dir} \
+                --target2={mniMask}'.format(gpu_num=subject_class.gpu_num,
+                           nocov_thal_roi = '{}/{}'.format(nocov_input_dir, nocov_img),
+                           bedpostDir = subject_class.bedpost_dir,
+                           reorient_mni2t1w2nodif = join(subject_class.reg_dir, 'mni2reorient_t1w2nodif_coeff.nii.gz'),
+                           reorient_nodif2t1w2mni = join(subject_class.reg_dir, 'reorient_nodif2t1w2mni_coeff.nii.gz'),
+                           tract_dir = nocov_tract_dir,
+                           mniMask = '/usr/local/fsl/data/standard/MNI152_T1_2mm_brain_mask.nii.gz')
+        
+            if not isfile(join(nocov_tract_dir, 'fdt_paths.nii.gz')):
+                commands.append(re.sub('\s+', ' ', command))
+        subject_class.commands = ';'.join(commands)
+        os.popen(subject_class.commands).read()
+
+#    for directory in [subject_class.segmentation_dir, subject_class.left_seg_dir, subject_class.right_seg_dir]:
+#        try:
+#            os.mkdir(directory)
+#        except:
+#            pass
+
+#    commands = []
+#    for sside, side in zip(['lh', 'rh'], ['left', 'right']):
+#        with open(join(subject_class.segmentation_dir, side, 'targets.txt'), 'w') as f:
+#            for cortex in cortices:
+#                f.write(join(subject_class.roi_dir, '{}_{}.nii.gz'.format(sside, cortex)) +'\n')
+
+#        command = 'CUDA_VISIBLE_DEVICES={gpu_num} /usr/local/fsl/bin/probtrackx2_gpu \
+#            -x {thal_roi} \
+#            -l \
+#            --onewaycondition \
+#            --omatrix2 \
+#            -c 0.2 \
+#            -S 2000 \
+#            --steplength=0.5 \
+#            -P 5000 \
+#            --fibthresh=0.01 \
+#            --distthresh=0.0 \
+#            --sampvox=0.0 \
+#            --forcedir \
+#            --opd \
+#            -s {bedpostDir}/merged \
+#            -m {bedpostDir}/nodif_brain_mask \
+#            --xfm={reorient_mni2t1w2nodif} \
+#            --invxfm=${reorient_nodif2t1w2mni} \
+#            --dir={outdir} \
+#            --target2={mniMask}'.format(gpu_num=subject_class.gpu_num,
+#                           thal_roi = join(subject_class.roi_dir, '{}_thalamus.nii.gz'.format(sside)),
+#                           bedpostDir = subject_class.bedpost_dir,
+#                           t12dti_flirtMat = join(subject_class.reg_dir, 'FREESURFERT1toNodif.mat'),
+#                           outdir = subject_class.out_dir,
+#                           mniMask = '/usr/local/fsl/data/standard/MNI152_T1_2mm_brain_mask.nii.gz')
+#        if not isfile(join(subject_class.segmentation_dir, side, 'fdt_paths.nii.gz')):
+#            commands.append(re.sub('\s+', ' ', command))
+#    subject_class.commands = ';'.join(commands)
+#    os.popen(subject_class.commands).read()
+
+
+#    for directory in [subject_class.out_dir, subject_class.output_dir]:
+#        try:
+#            print(directory)
+#        except:
+#            pass
+
+
+def run_probtrackx_parallel(subject_classes):
+    pool = Pool(processes=7)
+    # minimum number of commands in different GPUs
+    print(subject_classes.keys())
+    min_num = min([len(x) for x in subject_classes.values()])
+    print(min_num)
+    for num in range(min_num):
+        batch_subject_classes = [x[num] for x in subject_classes.values()]
+        pool.map(run_commands, batch_subject_classes)
+
+    batch_subject_classes = []
+    for i in subject_classes.values():
+        if len(i)>min_num:
+            batch_subject_classes += i
+    pool.map(run_commands, batch_subject_classes)
+
+if __name__ == '__main__':
+    data_dir = join(dirname(os.getcwd()), 'rsFC_to_wmSC')
+    subject_classes = [subject(join(data_dir, x)) for x in os.listdir(data_dir) if re.search('(FEP|NOR)\d+', x)]
+
+    for subject_class_tmp in subject_classes:
+        run_commands(subject_class_tmp)
+    # For multi-gpu support
+    new_subject_classes = {0:[], 1:[], 2:[],
+                           3:[], 4:[], 5:[], 6:[]}
+    gpu_num=0
+    for subject_class in subject_classes:
+        current_subject_list = new_subject_classes[gpu_num]
+        subject_class.gpu_num = gpu_num
+        print(subject_class.gpu_num)
+        current_subject_list += [subject_class]
+        new_subject_classes[gpu_num] = current_subject_list
+        if gpu_num == 6:
+            gpu_num = 0
+        else:
+            gpu_num+=1
+
+    #print([[y.gpu_num for y in x] for x in new_subject_classes.values()])
+    run_probtrackx_parallel(new_subject_classes)
+    #run_bedpostx(new_subject_classes)
+
+
+
---
+++ 40FEP_40NOR/6_rsfMRI_tractography_RelConn.py
@@ -0,0 +1,128 @@
+import os 
+import re
+import sys
+from os.path import join, basename, isfile, isdir
+import argparse
+import nibabel as nb
+import numpy as np
+import pandas as pd
+
+
+def threshold(voxelsize, side, smoothing, IC):
+
+    log = 'subjects.txt'
+    #log = 'test_subj.txt'
+    with open(log, 'r') as f:
+        subjects = f.read().split()
+
+    for subject in subjects:
+        
+        dataLoc = '{subject}/rsFC_seed_tractography/{voxelsize}_{side}_{smoothing}_{IC}ICs'.format(subject=subject, voxelsize=voxelsize, side=side, smoothing=smoothing, IC=IC)
+            
+        dataDirs = [ x for x in os.listdir(dataLoc) if '_demeaned_' in x]
+        for dataDir in dataDirs:
+            dataFile = '{dataLoc}/{dataDir}/fdt_paths.nii.gz'.format(dataLoc=dataLoc, dataDir=dataDir)
+            
+            for thr in range(10,30,10):
+                thresh_dataFile = '{dataLoc}/{dataDir}/{thr}_fdt_paths.nii.gz'.format(dataLoc=dataLoc, dataDir=dataDir, thr=thr)
+                if not os.path.isfile(thresh_dataFile):
+                    command = 'fslmaths {dataFile} -thrP {thr} {thresh_dataFile}'.format(dataFile=dataFile, thr=thr, thresh_dataFile=thresh_dataFile)
+                    os.popen(command).read
+
+
+
+def extractRelConn(voxelsize, side, smoothing, IC):
+    
+    #IC_connectivity = 'RelativeConnectivity_{voxelsize}_{side}_{smoothing}_{IC}IC.csv'.format(voxelsize=voxelsize, side=side, smoothing=smoothing, IC=IC)
+    IC_connectivity = 're_RelativeConnectivity_{voxelsize}_{side}_{smoothing}_{IC}IC.csv'.format(voxelsize=voxelsize, side=side, smoothing=smoothing, IC=IC)
+    
+    if not os.path.isfile(IC_connectivity):
+        
+        allData = []
+        
+        log = 'subjects.txt'
+        
+        #log = 'test_subj.txt'
+        with open(log, 'r') as f:
+            subjects = f.read().split()
+
+        imgInputs = []
+        for subject in subjects:
+            
+            dataLoc = '{subject}/rsFC_seed_tractography/{voxelsize}_{side}_{smoothing}_{IC}ICs'.format(subject=subject, voxelsize=voxelsize, side=side, smoothing=smoothing, IC=IC)
+            
+            dataDirs = [ x for x in os.listdir(dataLoc) if '_demeaned_' in x]
+            for dataDir in dataDirs:
+                dataFile = '{dataLoc}/{dataDir}/fdt_paths.nii.gz'.format(dataLoc=dataLoc, dataDir=dataDir)
+                imgInputs.append(dataFile)
+                for thr in range(10,30,10):
+                    thresh_dataFile = '{dataLoc}/{dataDir}/{thr}_fdt_paths.nii.gz'.format(dataLoc=dataLoc, dataDir=dataDir, thr=thr)
+                    imgInputs.append(thresh_dataFile)
+
+        for dataDir in dataDirs:
+            ic = dataDir.split("_")[1]
+            imgs = [x for x in imgInputs if '{ic}'.format(ic=ic) in x]
+            for subject in subjects:
+                subj_imgs = [x for x in imgs if '{subject}'.format(subject=subject) in x]
+                img = [x for x in subj_imgs if not '0_fdt' in x]
+                im = nb.load(''.join(img)).get_data()
+                ic_connectivity = sum(im[im != 0])
+
+                allData.append((subject, ic, 'None', ic_connectivity))
+                
+                for thr in range(10,30,10):
+                    thr_img = [x for x in subj_imgs if '{thr}_fdt'.format(thr=thr) in x]
+                    thr_im = nb.load(''.join(thr_img)).get_data()
+                    thr_ic_connectivity = sum(thr_im[thr_im != 0])
+                
+                    allData.append((subject, ic, thr, thr_ic_connectivity))
+            
+        
+        df = pd.DataFrame(allData)
+        df.columns = ['subject', 'IC_number', 'Threshold', 'IC_connectivity']
+
+        df.to_csv(IC_connectivity)
+
+    df = pd.read_csv(IC_connectivity, index_col=0)
+    df['side'] = '{side}'.format(side=side)
+    df['voxel_size'] = '{voxelsize}'.format(voxelsize=voxelsize)
+    df['spatial_smoothing'] = '{smoothing}'.format(smoothing=smoothing)
+    total_connectivity_sum = df.groupby(['subject', 'Threshold']).sum().reset_index()
+    total_connectivity_sum.columns = ['subject', 'Threshold', 'Total_connectivity']
+    new_df = pd.merge(df, total_connectivity_sum, on=['subject', 'Threshold'], how='inner')
+    new_df['Relative_connectivity'] = new_df.IC_connectivity / new_df.Total_connectivity
+    new_df['group'] = new_df['subject'].str[:3]
+    
+    new_df = new_df.sort_values(by=['IC_number', 'Threshold'])
+
+
+    new_df.to_csv(IC_connectivity)
+
+
+    #demo = pd.read_csv('37FEP_36HC_demo.csv')
+    #demo.columns = ['subject', 'sex', 'age']
+    #for_stats = pd.merge(new_df, demo, on=['subject'], how='inner')
+
+    #for_stats.to_csv(IC_connectivity)
+
+
+
+
+
+
+
+
+            
+            
+            
+
+if __name__== "__main__":
+	parser = argparse.ArgumentParser()
+	parser.add_argument('--voxelsize', '-vox', nargs=1, type=str)
+	parser.add_argument('--side', '-side', nargs=1, help = 'left or right', type=str)
+	parser.add_argument('--smoothing', '-smooth', nargs=1,  help = 'e.g., nosmooth, fwhm6, fwhm6preproc', type=str)
+	parser.add_argument('--IC', '-IC', nargs=1, help = 'e.g., 10 or 20', type=str)
+	args = parser.parse_args()
+
+	threshold(args.voxelsize[0], args.side[0], args.smoothing[0], args.IC[0])
+	extractRelConn(args.voxelsize[0], args.side[0], args.smoothing[0], args.IC[0])
